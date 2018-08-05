23fbf736557a7f8aeac06fa6683d0378ca7e3587
huangzhiwei <222667394@qq.com>
2018-08-06 02:16:42 +0800

    符合oa的 命名规范

--- src/com/dbPlug/sorm/utils/StringUtils.java
+++ src/com/dbPlug/sorm/utils/StringUtils.java
@@ -6,15 +6,16 @@ package com.dbPlug.sorm.utils;
  */
 public class StringUtils {
 	
-	/**
-	 * 将目标字符串首字母变为大写
-	 * @param str 目标字符串
-	 * @return 首字母变为大写的字符串
-	 */
+    /// aa_bb -> Aa_Bb
 	public static String firstChar2UpperCase(String str){
 		//abcd-->Abcd
-		//abcd-->ABCD-->Abcd
-		return str.toUpperCase().substring(0, 1)+str.substring(1);
+		String[] strings =  str.split("_");
+		String tableName = "";
+        for (String item : strings){
+        	tableName+=item.toUpperCase().substring(0, 1)+item.substring(1)+"_";
+		}
+		return  tableName.substring(0,tableName.length()-1);
 	}
+
 	
 }
